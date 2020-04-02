8e9dd00c243e39543c9975ed551b58c31faccbb9
David Walker <dwalker@calstate.edu>
2020-04-02 06:14:13 -0700

    campus group mapping

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,26 +8,31 @@ class Ability
   # Define any customized permissions here.
   def custom_permissions
 
-    campus = "bakersfield" if current_user.email.include?("bakersfield.edu")
-    campus = "chancellor" if current_user.email.include?("calstate.edu")
-    campus = "channel" if current_user.email.include?("ci.edu")
-    campus = "chico" if current_user.email.include?("chico.edu")
-    campus = "dominguez" if current_user.email.include?("dh.edu")
-    campus = "eastbay" if current_user.email.include?("eb.edu")
-    campus = "fresno" if current_user.email.include?("fresno.edu")
-    campus = "fullerton" if current_user.email.include?("fullerton.edu")
-    campus = "humboldt" if current_user.email.include?("humboldt.edu")
-    campus = "longbeach" if current_user.email.include?("lb.edu")
-    campus = "losangeles" if current_user.email.include?("la.edu")
-    campus = "maritime" if current_user.email.include?("maritime.edu")
-    campus = "mlml" if current_user.email.include?("mlml.edu")
-    campus = "northridge" if current_user.email.include?("northridge.edu")
-    campus = "pomona" if current_user.email.include?("bronco.edu")
-    campus = "sacramento" if current_user.email.include?("sacramento.edu")
-    campus = "sanfrancisco" if current_user.email.include?("sf.edu")
-    campus = "sanmarcos" if current_user.email.include?("sanmarcos.edu")
-    campus = "sonoma" if current_user.email.include?("sonoma.edu")
-    campus = "stanislaus" if current_user.email.include?("stanislaus.edu")
+    campus = "bakersfield" if current_user.campus == "csub"
+    campus = "chancellor" if current_user.campus == "co"
+    campus = "channel" if current_user.campus == "csuci"
+    campus = "chico" if current_user.campus == "csuchico"
+    campus = "dominguez" if current_user.campus == "csudh"
+    campus = "eastbay" if current_user.campus == "csueastbay"
+    campus = "fresno" if current_user.campus == "csufresno"
+    campus = "fullerton" if current_user.campus == "fullerton"
+    campus = "humboldt" if current_user.campus == "humboldt"
+    campus = "longbeach" if current_user.campus == "csulb"
+    campus = "losangeles" if current_user.campus == "calstatela"
+    campus = "maritime" if current_user.campus == "csum"
+    campus = "monterey" if current_user.campus == "csumb"
+    campus = "mlml" if current_user.campus == "mlml"
+    campus = "northridge" if current_user.campus == "csun"
+    campus = "pomona" if current_user.campus == "csupomona"
+    campus = "sacramento" if current_user.campus == "csus"
+    campus = "sanbernardino" if current_user.campus == "csusb"
+    campus = "sandiego" if current_user.campus == "sdsu"
+    campus = "sanfrancisco" if current_user.campus == "sfsu"	
+    campus = "sanjose" if current_user.campus == "sjsu"	
+    campus = "slo" if current_user.campus == "calpoly"	
+    campus = "sanmarcos" if current_user.campus == "csusm"	
+    campus = "sonoma" if current_user.campus == "sonoma"	
+    campus = "stanislaus" if current_user.campus == "csustan"
 
     user_groups.push(campus)
 
